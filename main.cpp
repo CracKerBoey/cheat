38faf5dbb05f8406594bbc6df93248c42a4ccd43
CracKerBoey <92775624+CracKerBoey@users.noreply.github.com>
2021-10-20 17:53:41 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,228 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"hello world"<<endl;
+    cout<<"next line";
+    // short s54655rra=55;
+    // short sa=55;
+    // cout<<s54655rra;
+
+    // camelcase notation
+    // int marksInmaths=90;
+    // cout<<"my marks in maths is "<<marksInmaths;
+    // short a;
+    // int b;
+    // long c;
+    // long long d;
+    //    float const score=39.7755;
+    // score = 99.88;
+    // cout<< score;
+    // score = 99.88;
+
+    // int a, b;
+    // cout<<"enter first no."<<endl;
+    // cin>>a;
+    // cout<<"enter second no."<<endl;
+    // cin>>b;
+
+    // cout<<"a+b is"<< a+b<<endl;
+    // cout<<"a-b is"<< a-b<<endl;
+    // cout<<"a*b is"<< a*b<<endl;
+    // cout<<"a/b is"<<(float) a/b<<endl;
+
+    // // if else conditional
+
+    // int age;
+    // cout<<"what is your age"<<endl;
+    // cin>>age;
+    // if(age>90 || age<1)
+    // {
+    //     cout<<"invalid age";
+    // }
+
+    // else if(age>=17)
+    // {
+    //     cout<<"you cannot ride";
+    // }
+    // else if(age=14)
+    // {
+    //     cout<<"you cannot ride";
+    // }
+
+    // else
+    // {
+    //     cout<< "you can ride";
+    // }
+
+    // switch statement
+    // int year;
+    // cout<<"what ir yore year of birth:";
+    // cin>> year;
+    // switch (year)
+    // {
+    // case 1995:
+    //     cout<<"you will be 27 years old next year";
+    //     break;
+    // case 1996:
+    //     cout<<"you will be 26 years old next year";
+    //     break;
+    // case 1997:
+    //     cout<<"you will be 25 years old next year";
+    //     break;
+    // case 1998:
+    //     cout<<"you will be 24 years old next year";
+    //     break;
+    // case 1999:
+    //     cout<<"you will be 23 years old next year";
+    //     break;
+    // case 2000:
+    //     cout<<"you will be 22 years old next year";
+    //     break;
+    // case 2001:
+    //     cout<<"you will be 21 years old next year";
+    //     break;
+    // case 2002:
+    //     cout<<"you will be 20 years old next year";
+    //     break;
+    // case 2003:
+    //     cout<<"you will be 19 years old next year";
+    //     break;
+    // case 2004:
+    //     cout<<"you will be 18 years old next year";
+    //     break;
+    // case 2005:
+    //     cout<<"you will be 17 years old next year";
+    //     break;
+    // case 2006:
+    //     cout<<"you will be 16 years old next year";
+    //     break;
+    // case 2007:
+    //     cout<<"you will be 15 years old next year";
+    //     break;
+    // case 2008:
+    //     cout<<"you will be 14 years old next year";
+    //     break;
+    // case 2009:
+    //     cout<<"you will be 13 years old next year";
+    //     break;
+    // case 2010:
+    //     cout<<"you will be 12 years old next year";
+    //     break;
+    // default:
+    //     cout<< "Sorry for inconvinence,I cannot determine your age.";
+    //     break;
+    // }
+
+    // loops
+
+    // int i = 0;
+
+    // while loop
+
+    // while (i<=98)
+    // {
+    // cout<<"We are at room no."<<i<<endl;
+    // i=i+2;
+    // }
+
+    // do while loop
+
+    // do
+    // {
+    //     cout << "We are at room no." << i << endl;
+    //     i = i + 2;
+
+    // } while (i>55);
+
+    // for loop
+
+    // for (int i = 0; i <= 95; i++)
+    // {
+    //     cout<<"The value of i is "<<i<<endl;
+
+    // }
+
+    // Array
+
+    // int arr[] = {5,6,7};
+    //  for (int i = 1; i <4; i++)
+    // {
+    //     cout<<"The marks of "<<i<<"th student is "<<arr[i-1]<<endl;
+    // }
+
+    // 2d Array
+
+    // int arr2d[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+    // for (int i = 1; i < 2; i++)
+    // {
+    //     for (int j = 1; j < 4; j++)
+    //     {
+    //         cout << "|" << arr2d[i - 1][j - 1] << "|";
+    //     }
+    // }
+    // cout << endl;
+
+    // for (int i = 2; i < 3; i++)
+    // {
+    //     for (int j = 1; j < 4; j++)
+    //     {
+    //         cout << "|" << arr2d[i - 1][j - 1] << "|";
+    //     }
+    //     cout << endl;
+    // }
+    // for (int i = 3; i < 4; i++)
+    // {
+    //     for (int j = 1; j < 4; j++)
+    //     {
+    //         cout << "|" << arr2d[i - 1][j - 1] << "|";
+    //     }
+    // }
+
+    // trying to make 3x3 matrix with array
+
+    // cout << endl;
+    // cout << "|" << arr2d[0][0] << " ";
+    // cout << arr2d[0][1] << " ";
+    // cout << arr2d[0][2] << "|";
+    // cout << endl;
+    // cout << "|" << arr2d[1][0] << " ";
+    // cout << arr2d[1][1] << " ";
+    // cout << arr2d[1][2] << "|";
+    // cout << endl;
+    // cout << "|" << arr2d[2][0] << " ";
+    // cout << arr2d[2][1] << " ";
+    // cout << arr2d[2][2] << "|";
+
+    // pta nhi kya h par kaam ka h
+
+    // int a=100;
+    // float z=99.99;
+    // cout<<(float) a/3<<endl;
+    // cout<<(int) z;
+
+    //string
+    // #include <string>
+
+    // string name = "Ankit";
+    // cout << "the name is " << name << endl;
+    // cout << "the name is " << name.substr(0, 3) << endl;
+    // cout << "the length of name is " << name.length() << endl;
+
+    // pointers
+
+    // int a=99;
+    // int* ptra;
+    // ptra = &a;
+    // cout<< "the value of a is "<<a<<endl;
+    // cout<< "the value of a is "<<*ptra<<endl;
+    // cout<< "the address of a is "<<&a<<endl;
+    // cout<< "the address of a is "<<ptra<<endl;
+
+    // classes and object
+    //  yaad h toh likh de
+
+    return 0;
+}
